7e722afc2da383380fb5aef4fd5aabd8f6121ebf
marcoslopez <marcoslopezsanchez@hotmail.com>
2021-08-09 09:40:58 -0500

    lodash
    instalación de paquetes de lodash.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { isEmpty } from 'lodash'
 
 function App() {
+    const [task, setTask] = useState(null)
+
+    const addTask = (e) => {
+      e.preventDefault()
+      if (isEmpty(task)){
+        console.log("Task empty")
+        return
+      }
+    console.log("ok")
+    setTask("")
+
+    }
+
   return (
-    <div className = "container mt-5">
-        <h1> Hola Mundo! </h1>
-    </div>
+    <div className="container mt-5">
+        <h1>Tareas</h1>
+      <hr/>
+        <div className="row">
+          <div className="col-8">
+            <h4 className="text-center">Lista de Tareas</h4>
+            <ul className="list-group">
+              <li className="list-group-item">
+                <span className="lead">Nombre de la Tarea</span>
+                <button className="btn btn-danger btn-sm float-right mx-2">Eliminar</button>
+                <button className="btn btn-warning btn-sm float-right">Editar</button>
+              </li>
+            </ul>
+          </div>
+          <div className="col-4">
+            <h4 className="text-center">Formulario</h4>
+            <form onSubmit={addTask}>
+                <input
+                  type="text"
+                  className="form-control mb-2"
+                  placeholder="Ingrese las Tareas...."
+                  onChange={(text) => setTask(text.target.value)}
+                  value={task}                
+                />
+              <button 
+                className="btn btn-dark btn-block" 
+                type="submit"
+                >
+                Agregar
+              </button>  
+            </form>           
+          </div>
+        </div>
+      </div>
   )
 }
 
